1313d7067c364e010a6f1a9548c6d1a41c545ed8
Martin Duhem <martin.duhem@gmail.com>
2018-02-10 21:33:04 +0100

    Blindly trying to configure Travis

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val bloopVersion = "1.0.0-M1"
+  val bloopVersion = "1.0.0-M3"
   val metaconfigVersion = "0.6.0"
 
   val bloopFrontend = "ch.epfl.scala" %% "bloop-frontend" % bloopVersion
